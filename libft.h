59a0862f03575029bdfa14fde8b479b3c5028671
Attila Istok <aistok@c1r3s7.42london.com>
2024-04-08 19:09:01 +0100

    ft_strlen.c work in progress, the rest were tested and seem OK :)

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aistok <aistok@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 16:45:57 by aistok            #+#    #+#             */
+/*   Updated: 2024/04/08 19:05:23 by aistok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <aio.h>
+/*!!! ifndef...*/
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t  ft_strlen(const char *s);
+/*
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+*/
