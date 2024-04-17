8adc7c7ae678fe0a3ee85bcd61eb1effe9795f4b
Attila Istok <aistok@c1r3s7.42london.com>
2024-04-17 23:00:56 +0100

    satisfied norminette

--- libft.h
+++ libft.h
@@ -6,22 +6,21 @@
 /*   By: aistok <aistok@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:45:57 by aistok            #+#    #+#             */
-/*   Updated: 2024/04/12 17:26:59 by aistok           ###   ########.fr       */
+/*   Updated: 2024/04/17 23:00:14 by aistok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <aio.h>
-
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <aio.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 
-size_t  ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
