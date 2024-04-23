731cf63db956383c46e53a85f50435ed80559bff
Attila Istok <aistok@c3r2s4.42london.com>
2024-04-23 20:56:42 +0100

    ft_strchr.c - added commented description

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,10 +6,21 @@
 /*   By: aistok <aistok@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:16:38 by aistok            #+#    #+#             */
-/*   Updated: 2024/04/18 18:14:50 by aistok           ###   ########.fr       */
+/*   Updated: 2024/04/23 20:44:44 by aistok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ *	DESCRIPTION
+ *	The  strchr() function returns a pointer to the first occurrence of the
+ *	character c in the string s.
+ *
+ *	RETURN VALUE
+ *	The strchr() and strrchr() functions return a pointer  to  the  matched
+ *	character  or NULL if the character is not found.  The terminating null
+ *	byte is considered part of the string, so that if  c  is  specified  as
+ *	'\0', these functions return a pointer to the terminator.
+ */
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s != 0)
