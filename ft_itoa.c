4c7ac757fb9b176ee7755ea5a990fd1d9f0cd5f9
Attila Istok <aistok@c3r1s6.42london.com>
2024-04-27 21:05:48 +0100

    ft_itoa.c is done, tested and norminette OK

---
+++ ft_itoa.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aistok <aistok@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 01:34:33 by aistok            #+#    #+#             */
+/*   Updated: 2024/04/27 21:03:02 by aistok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* used for malloc */
+#include <stdlib.h>
+/* used for NULL */
+#include <aio.h>
+
+int	length(int n)
+{
+	int	len;
+
+	len = 1 + (n < 0);
+	while (n / 10 != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+/*
+ *	Description
+ *	Allocates (with malloc(3)) and returns a string representing the integer
+ *	received as an argument. Negative numbers must be handled.
+ *
+ *	Return value
+ *	The string representing the integer.
+ *	NULL if the allocation fails.
+ */
+char	*ft_itoa(int n)
+{
+	int		len;
+	char	*str;
+
+	len = length(n);
+	str = (char *)malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	str[0] = '-';
+	if (n == 0)
+		str[0] = '0';
+	str[len] = 0;
+	while (n != 0)
+	{
+		if (n < 0)
+			str[--len] = (n % 10) * (-1) + 48;
+		else
+			str[--len] = n % 10 + 48;
+		n = n / 10;
+	}
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aistok <aistok@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 16:45:57 by aistok            #+#    #+#             */
-/*   Updated: 2024/04/25 17:13:38 by aistok           ###   ########.fr       */
+/*   Updated: 2024/04/27 21:04:32 by aistok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,9 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char    **ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 
+char	*ft_itoa(int n);
 int		ft_atoi(const char *nptr);
 
 void	*ft_memset(void *s, int c, size_t n);
