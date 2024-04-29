8433279a988013d34917cf09675fb05496fa4009
Attila Istok <aistok@c1r2s5.42london.com>
2024-04-30 00:59:56 +0100

    the function prototype returned void instead of void* and there was no return at the end of the function...!!! All fixed!

--- ft_memset.c
+++ ft_memset.c
@@ -6,19 +6,21 @@
 /*   By: aistok <aistok@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:31:18 by aistok            #+#    #+#             */
-/*   Updated: 2024/04/17 18:42:06 by aistok           ###   ########.fr       */
+/*   Updated: 2024/04/30 00:42:01 by aistok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* used for size_t */
 #include <aio.h>
+/* used for NULL */
+#include <stddef.h>
 
-void	ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*src;
 
 	if (!s)
-		return ;
+		return (NULL);
 	src = (unsigned char *)s;
 	while (n > 0)
 	{
@@ -26,4 +28,5 @@ void	ft_memset(void *s, int c, size_t n)
 		src++;
 		n--;
 	}
+	return (s);
 }
